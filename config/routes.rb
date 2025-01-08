374766220a4fee355b869a09c5ecbc122a1e37ed
Sitthata <aprils96950@gmail.com>
2025-01-08 15:49:16 +0700

    Add inventory display and subscription functionality for products

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,9 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-  resources :products
+  resources :products do
+    resource :subscribers, only: [ :create ]
+  end
   # Defines the root path route ("/")
   root "products#index"
-
 end
