741c40c81e1a0b2ab4c1443d95f89cb087923923
Sitthata <aprils96950@gmail.com>
2025-01-08 14:12:04 +0700

    add riches text description to view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: %i[show edit update destroy]
   allow_unauthenticated_access only: %i[ index show ]
-  
+
   def index
     @products = Product.all
   end
@@ -58,6 +58,6 @@ class ProductsController < ApplicationController
       @product = Product.find(params[:id])
     end
     def product_params
-      params.expect(product: [ :name ])
+      params.expect(product: [ :name, :description ])
     end
 end
