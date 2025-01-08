5b4adbd30a57226ae380b2ec700b7b4f8b551082
Sitthata <aprils96950@gmail.com>
2025-01-08 13:47:37 +0700

    Add basic authentication and logout feature

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,8 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
+  resources :products
   # Defines the root path route ("/")
   root "products#index"
 
-  resources :products
 end
