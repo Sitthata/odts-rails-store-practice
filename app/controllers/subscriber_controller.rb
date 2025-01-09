765f48814c1f27159a245d5cee0d2bd655136c43
Sitthata <aprils96950@gmail.com>
2025-01-09 08:33:39 +0700

    Add subscriber creation with product association in controller

--- app/controllers/subscriber_controller.rb
+++ app/controllers/subscriber_controller.rb
@@ -1,6 +1,9 @@
 class SubscriberController < ApplicationController
   allow_unauthenticated_access
+  before_action :set_product
   def create
+    @product.subscribers.where(subscriber_params).first_or_create
+    redirect_to @product, notice: "You are now subscribed."
   end
 
   private
