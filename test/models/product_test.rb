602e6335ebd7d5b3275372689f0167ce9dce510d
Sitthata <aprils96950@gmail.com>
2025-01-09 09:45:28 +0700

    Update product and subscriber fixtures; enhance product mailer tests for inventory notifications

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
+  include ActionMailer::TestHelper
   # test "the truth" do
   #   assert true
   # end
+  test "Send email notification when product is in stock" do
+    product = products(:tshirt)
+
+    product.update(inventory_count: 0)
+
+    assert_emails 2 do
+      product.update(inventory_count: 99)
+    end
+  end
 end
